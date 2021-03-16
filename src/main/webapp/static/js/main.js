4542dd44d43e7e4b95bb8acdec4a51d096f0b966
blippard <blippard@gmail.com>
2021-03-16 15:57:41 +0100

    Edit: Js builds products onto sidebar after add-to-cart clicked

--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -10,8 +10,18 @@ function addToCartButtonListener() {
             let productName = btn.name;
             let productPrice = btn.parentElement.parentElement.firstElementChild.innerText;
             let quantity = 1;
-            
-
+            let sidebar = document.querySelector('.sidenav');
+            let cartProductDiv = `
+                        <div class="cart-item">
+                            <div>${productName}</div>
+                            <div>${productPrice}</div>
+                            <div>${quantity}</div>
+                            <div class="plus-minus">
+                                <div class="quant-plus">+</div>/<div class="quant-minus">-</div>
+                            </div> 
+                        </div>`;
+                sidebar.style.display = "flex";
+                sidebar.insertAdjacentHTML("beforeend", cartProductDiv);
 
             }
         ))
