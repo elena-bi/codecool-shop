eb5f427cc5a75e276463122182a67e2ac737e316
blippard <blippard@gmail.com>
2021-03-16 15:26:24 +0100

    Add: listener to each add to cart button

--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -1,9 +1,12 @@
 function init() {
 
+    addToCartButtonListener()
 }
 
-
-
-
+function addToCartButtonListener() {
+    document.querySelectorAll('.btn.btn-success')
+        .forEach(btn => btn.addEventListener("click",
+            () => console.log(btn)))
+}
 
 init()
