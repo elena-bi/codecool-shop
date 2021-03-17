9ed768feaf512b1b17ab0ca6bc65cb1d23947cab
blippard <blippard@gmail.com>
2021-03-17 20:27:27 +0100

    Edit: catch form request in post method

--- src/main/java/com/codecool/shop/controller/CheckoutController.java
+++ src/main/java/com/codecool/shop/controller/CheckoutController.java
@@ -1,11 +1,40 @@
 package com.codecool.shop.controller;
 
+import com.codecool.shop.config.TemplateEngineUtil;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-@WebServlet(urlPatterns = {"/cart"})
+@WebServlet(urlPatterns = {"/Checkout"})
 public class CheckoutController extends HttpServlet {
 
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        String userName = request.getParameter("name");
+        String userEmailAddress = request.getParameter("email-address");
+        String userDeliveryAddress = request.getParameter("delivery-address");
+        String phoneNumber = request.getParameter("phone-number");
+        String bankName = request.getParameter("bank");
+        String userCardNumber = request.getParameter("card-number");
+
+        System.out.println(userName + ", " + userEmailAddress + ", " + userDeliveryAddress + ", " + phoneNumber + ", " + bankName + ", " + userCardNumber);
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
+        WebContext context = new WebContext(request, response, request.getServletContext());
+        engine.process("Checkout.html", context, response.getWriter());
+
+    }
 
 }
