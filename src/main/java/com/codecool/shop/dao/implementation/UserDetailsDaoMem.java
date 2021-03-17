dc7c5222e59916e28efa6f846ffeadeaf0783ad3
blippard <blippard@gmail.com>
2021-03-17 20:39:44 +0100

    Add: instance method and getters and setters for all attirbutes

--- src/main/java/com/codecool/shop/dao/implementation/UserDetailsDaoMem.java
+++ src/main/java/com/codecool/shop/dao/implementation/UserDetailsDaoMem.java
@@ -1,4 +1,77 @@
 package com.codecool.shop.dao.implementation;
 
-public class UserDetailsDaoMem {
+import com.codecool.shop.dao.UserDetailsDao;
+
+public class UserDetailsDaoMem implements UserDetailsDao {
+    private String userName;
+    private String userEmailAddress;
+    private String userDeliveryAddress;
+    private String phoneNumber;
+    private String bankName;
+    private String userCardNumber;
+    private static UserDetailsDaoMem instance;
+
+    private UserDetailsDaoMem() {}
+
+    public static UserDetailsDaoMem getInstance() {
+        if (instance == null) {
+            instance = new UserDetailsDaoMem();
+        }
+        return instance;
+    }
+
+    @Override
+    public String getUserName() {
+        return userName;
+    }
+
+    @Override
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    @Override
+    public String getUserEmailAddress() {
+        return userEmailAddress;
+    }
+
+    @Override
+    public void setUserEmailAddress(String userEmailAddress) {
+        this.userEmailAddress = userEmailAddress;
+    }
+
+    @Override
+    public String getUserDeliveryAddress() {
+        return userDeliveryAddress;
+    }
+
+    @Override
+    public void setUserDeliveryAddress(String userDeliveryAddress) {
+        this.userDeliveryAddress = userDeliveryAddress;
+    }
+
+    @Override
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    @Override
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String getBankName() {
+        return bankName;
+    }
+
+    @Override
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    @Override
+    public String getUserCardNumber() {
+        return userCardNumber;
+    }
 }
