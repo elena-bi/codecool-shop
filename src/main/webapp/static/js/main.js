9cb41f56baebdb1362ff661596bec51539d30760
blippard <blippard@gmail.com>
2021-03-17 09:20:40 +0100

    Edit: added product ID to cart items

--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -7,16 +7,20 @@ function addToCartButtonListener() {
     document.querySelectorAll('.btn.btn-success')
         .forEach(btn => btn.addEventListener("click",
             () => {
+            // add product ID
             let productName = btn.name;
             let productPrice = btn.parentElement.parentElement.firstElementChild.innerText;
             let quantity = 1;
+            let productId = btn.id;
             let sidebar = document.querySelector('.sidenav');
+            // add product ID, and search if item already in cart
             let cartProductDiv = `
-                        <div class="cart-item">
+                        <div class="cart-item" id="${productId}">
                             <div class="product-name">${productName}</div>
                             <div class="product-price">${productPrice}</div>
                             <div class="quantity">${quantity}<a>+</a><a>-</a></div>
                         </div>`;
+                // add + / - separately with event listeners
                 sidebar.style.display = "block";
                 sidebar.insertAdjacentHTML("beforeend", cartProductDiv);
 
