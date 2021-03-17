ca086ce2a3774e2a4f1d5837a0a5e6fcc8a167c2
Solya Gergo <solya.gergo.sandor@gmail.com>
2021-03-17 15:40:35 +0100

    Add: RequestToJsonObject utility class

--- src/main/java/com/codecool/shop/controller/PayPalController.java
+++ src/main/java/com/codecool/shop/controller/PayPalController.java
@@ -1,6 +1,6 @@
 package com.codecool.shop.controller;
 
-import com.google.gson.Gson;
+import com.codecool.shop.util.RequestToJsonObject;
 import com.google.gson.JsonObject;
 
 import javax.servlet.ServletException;
@@ -8,25 +8,14 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 @WebServlet(urlPatterns = {"/payment/paypal"})
 public class PayPalController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(req.getInputStream()));
-        String inputLine;
-        StringBuffer content = new StringBuffer();
-        while ((inputLine = br.readLine()) != null) {
-            content.append(inputLine);
-        }
-        br.close();
-        System.out.println(content);
-        Gson gson = new Gson();
-        JsonObject jsonObj = gson.fromJson(content.toString(), JsonObject.class);
+        JsonObject jsonObj = RequestToJsonObject.getJsonObjectFromRequest(req);
         boolean isTransactionSuccessful;
         if (jsonObj.get("username") == null || jsonObj.get("password") == null) {
             isTransactionSuccessful = false;
