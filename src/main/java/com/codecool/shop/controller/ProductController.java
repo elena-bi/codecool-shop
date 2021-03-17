09ae9a68b06f8d2f3ab3b9e3b76103bfdbc894bd
elena-bi <bisericescuelena@gmail.com>
2021-03-17 16:29:54 +0200

    filtering by suppliers and category feature

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -26,6 +26,23 @@ import java.util.Map;
 @WebServlet(urlPatterns = {"/"})
 public class ProductController extends HttpServlet {
 
+//    @Override
+//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        ProductDao productDataStore = ProductDaoMem.getInstance();
+//        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+//
+//        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+//        WebContext context = new WebContext(req, resp, req.getServletContext());
+//        context.setVariable("category", productCategoryDataStore.find(1));
+//        context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(1)));
+//        // // Alternative setting of the template context
+//        // Map<String, Object> params = new HashMap<>();
+//        // params.put("category", productCategoryDataStore.find(1));
+//        // params.put("products", productDataStore.getBy(productCategoryDataStore.find(1)));
+//        // context.setVariables(params);
+//        engine.process("product/index.html", context, resp.getWriter());
+//    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String categoryIdAsString;
