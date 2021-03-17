494f91b51e1c628f5b139d3c37f8b79e22c4b291
blippard <blippard@gmail.com>
2021-03-17 14:37:42 +0100

    Edit: cart controller updated

--- src/main/java/com/codecool/shop/controller/CartController.java
+++ src/main/java/com/codecool/shop/controller/CartController.java
@@ -3,6 +3,8 @@ package com.codecool.shop.controller;
 import com.codecool.shop.config.TemplateEngineUtil;
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.implementation.CartDao;
+import com.codecool.shop.dao.implementation.CartDaoMem;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
 import com.google.gson.JsonArray;
@@ -29,19 +31,18 @@ public class CartController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+            CartDao shoppingCart = CartDaoMem.getInstance();
 
-//            ProductDao productDataStore = ProductDaoMem.getInstance();
-//            ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
-//
-//            TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
-//            WebContext context = new WebContext(req, resp, req.getServletContext());
-//            context.setVariable("category", productCategoryDataStore.find(1));
-//            context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(1)));
-//            // // Alternative setting of the template context
-//            // Map<String, Object> params = new HashMap<>();
-//            // params.put("category", productCategoryDataStore.find(1));
-//            // params.put("products", productDataStore.getBy(productCategoryDataStore.find(1)));
-//            // context.setVariables(params);
-//            engine.process("product/index.html", context, resp.getWriter());
+
+            TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+            WebContext context = new WebContext(req, resp, req.getServletContext());
+            context.setVariable("category", productCategoryDataStore.find(1));
+            context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(1)));
+            // // Alternative setting of the template context
+            // Map<String, Object> params = new HashMap<>();
+            // params.put("category", productCategoryDataStore.find(1));
+            // params.put("products", productDataStore.getBy(productCategoryDataStore.find(1)));
+            // context.setVariables(params);
+            engine.process("cart.html", context, resp.getWriter());
     }
 }
