b34db170d075824a255fe105c4b25bf00cdb36a2
blippard <blippard@gmail.com>
2021-03-17 14:27:14 +0100

    Edit: modified buffer to read into hashmap

--- src/main/java/com/codecool/shop/servlets/CartServlet.java
+++ src/main/java/com/codecool/shop/servlets/CartServlet.java
@@ -1,4 +1,42 @@
 package com.codecool.shop.servlets;
 
-public class CartServlet {
+
+import com.google.gson.JsonArray;
+
+import javax.script.ScriptContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.codecool.hackernews.JsonHandler;
+
+@WebServlet(urlPatterns = {"/api/cart"})
+public class CartServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HashMap<Integer, Integer> cartMap = new HashMap<>();
+        BufferedReader reader = req.getReader();
+        String message = reader.lines().collect(Collectors.joining(","));
+        for (int i = 0; i < message.length() - 2; i+=4) {
+            cartMap.put((int) message.charAt(i) - 48, (int) message.charAt(i + 2) - 48);
+        }
+
+    }
 }
