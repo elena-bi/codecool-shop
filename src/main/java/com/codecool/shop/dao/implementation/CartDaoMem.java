4a24621f7daf3bdc91c0f59c47d8b89603cd7aeb
blippard <blippard@gmail.com>
2021-03-17 14:28:51 +0100

    Interface: Cart Dao interface added

---
+++ src/main/java/com/codecool/shop/dao/implementation/CartDaoMem.java
@@ -0,0 +1,30 @@
+package com.codecool.shop.dao.implementation;
+
+import com.codecool.shop.model.ProductCategory;
+
+import java.util.HashMap;
+
+public class CartDaoMem implements CartDao{
+    private HashMap<Integer, Integer> cartMap;
+    private static CartDaoMem instance = null;
+
+    private CartDaoMem() {}
+
+    public static CartDaoMem getInstance() {
+        if (instance == null) {
+            instance = new CartDaoMem();
+        }
+        return instance;
+    }
+
+    @Override
+    public void setCartMap(HashMap<Integer, Integer> cartMap) {
+        this.cartMap = cartMap;
+    }
+
+    @Override
+    public HashMap<Integer, Integer> getCartMap(){
+        return this.cartMap;
+    }
+
+}
