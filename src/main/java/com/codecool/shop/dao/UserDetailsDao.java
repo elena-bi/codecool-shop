5d493433d3a0ca73264ea2d02c5517039672afc9
blippard <blippard@gmail.com>
2021-03-17 20:39:23 +0100

    Add: interface methods added

--- src/main/java/com/codecool/shop/dao/UserDetailsDao.java
+++ src/main/java/com/codecool/shop/dao/UserDetailsDao.java
@@ -1,4 +1,25 @@
 package com.codecool.shop.dao;
 
 public interface UserDetailsDao {
+    String getUserName();
+
+    void setUserName(String userName);
+
+    String getUserEmailAddress();
+
+    void setUserEmailAddress(String userEmailAddress);
+
+    String getUserDeliveryAddress();
+
+    void setUserDeliveryAddress(String userDeliveryAddress);
+
+    String getPhoneNumber();
+
+    void setPhoneNumber(String phoneNumber);
+
+    String getBankName();
+
+    void setBankName(String bankName);
+
+    String getUserCardNumber();
 }
