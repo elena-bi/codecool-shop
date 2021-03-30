4e5424f115782619345981ec4200343ca096cc94
blippard <blippard@gmail.com>
2021-03-30 11:45:09 +0200

    Add: method to connect to db using config file

--- src/main/java/com/codecool/shop/dao/database/DatabaseManager.java
+++ src/main/java/com/codecool/shop/dao/database/DatabaseManager.java
@@ -1,4 +1,33 @@
 package com.codecool.shop.dao.database;
 
+import com.codecool.shop.util.ReadPropertyValues;
+import org.postgresql.ds.PGSimpleDataSource;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.HashMap;
+
 public class DatabaseManager {
+
+    private DataSource connect() throws SQLException, IOException {
+        ReadPropertyValues properties = new ReadPropertyValues();
+        HashMap<String, String> config = properties.getPropertyValues();
+
+        PGSimpleDataSource dataSource = new PGSimpleDataSource();
+        String dbName = config.get("database");
+        String user = config.get("user");
+        String password = System.getenv("MY_PSQL_PASSWORD");
+
+        dataSource.setDatabaseName(dbName);
+        dataSource.setUser(user);
+        dataSource.setPassword(password);
+
+        System.out.println("Trying to connect");
+        dataSource.getConnection().close();
+        System.out.println("Connection ok.");
+
+        return dataSource;
+    }
+
 }
