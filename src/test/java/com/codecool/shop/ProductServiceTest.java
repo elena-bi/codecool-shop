9e3b974b414fedc185d3c118ccd155a669b7184b
blippard <blippard@gmail.com>
2021-03-31 12:55:54 +0200

    Directory: Created test directory and packages

---
+++ src/test/java/com/codecool/shop/ProductServiceTest.java
@@ -0,0 +1,28 @@
+package com.codecool.shop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ProductServiceTest {
+
+    @Test
+    void getProductCategory_correctID_returnsID() {
+
+    }
+
+    @Test
+    void getProductCategory_incorrectID_throwsError() {
+
+    }
+
+    @Test
+    void getProductsForCategory_existingCategory_returnsProducts() {
+
+    }
+
+    @Test
+    void getProductsForCategory_incorrectCategory_throwsError() {
+
+    }
+}
