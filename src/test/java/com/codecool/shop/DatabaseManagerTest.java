ce993fce21c2907d13000748234ca9013009c08d
blippard <blippard@gmail.com>
2021-03-31 14:16:21 +0200

    Class: DatabaseManagerTest class created

---
+++ src/test/java/com/codecool/shop/DatabaseManagerTest.java
@@ -0,0 +1,14 @@
+package com.codecool.shop;
+
+import com.codecool.shop.dao.database.DatabaseManager;
+import org.junit.jupiter.api.Test;
+
+public class DatabaseManagerTest {
+
+    @Test
+    void connect_Initiated_ConnectsCorrectly() {
+        DatabaseManager dbManagerTest = new DatabaseManager();
+
+
+    }
+}
