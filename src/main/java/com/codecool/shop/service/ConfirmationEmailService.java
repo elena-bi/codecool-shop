11d8dbffc982aeb8110efbd9a85cb1f4e5548189
Solya Gergo <solya.gergo.sandor@gmail.com>
2021-03-31 11:36:07 +0200

    Add: ConfirmationEmailService sendRegistrationConfirmationEmail

--- src/main/java/com/codecool/shop/service/ConfirmationEmailService.java
+++ src/main/java/com/codecool/shop/service/ConfirmationEmailService.java
@@ -9,12 +9,15 @@ import com.codecool.shop.dao.implementation.UserDetailsDaoMem;
 import com.codecool.shop.model.Product;
 
 import javax.mail.*;
+import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.util.Map;
 import java.util.Properties;
 
 public class ConfirmationEmailService {
+    private final String FROM_NAME = "fillertext@gmail.com"; // Change to valid email to use
+    private final String FROM_PASSWORD = "password";         // Change to valid password to use
     private static ConfirmationEmailService instance = null;
 
     private ConfirmationEmailService() {}
@@ -25,27 +28,30 @@ public class ConfirmationEmailService {
         }
         return instance;
     }
-    public void sendOrderConfirmationEmail() {
-        CartDao cart = CartDaoMem.getInstance();
-        UserDetailsDao userDetails = UserDetailsDaoMem.getInstance();
-        ProductDao productList = ProductDaoMem.getInstance();
-        String fromName = "fillertext@gmail.com"; // Change to valid email to use
-        String fromPassword = "password";         // Change to valid password to use
 
-        String to = userDetails.getUserEmailAddress();
+    private Session setupSession() {
         Properties prop = new Properties();
         prop.put("mail.smtp.host", "smtp.gmail.com");
         prop.put("mail.smtp.port", "587");
         prop.put("mail.smtp.auth", "true");
         prop.put("mail.smtp.starttls.enable", "true");
 
-        Session session = Session.getInstance(prop,
+        return Session.getInstance(prop,
                 new Authenticator() {
                     @Override
                     protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication(fromName, fromPassword);
+                        return new PasswordAuthentication(FROM_NAME, FROM_PASSWORD);
                     }
                 });
+    }
+
+    public void sendOrderConfirmationEmail() {
+        CartDao cart = CartDaoMem.getInstance();
+        UserDetailsDao userDetails = UserDetailsDaoMem.getInstance();
+        ProductDao productList = ProductDaoMem.getInstance();
+        String to = userDetails.getUserEmailAddress();
+
+        Session session = setupSession();
 
         StringBuilder sb = new StringBuilder("Buyer: ");
         sb.append(userDetails.getUserName())
@@ -77,10 +83,24 @@ public class ConfirmationEmailService {
 
         try {
             MimeMessage message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(fromName));
+            message.setFrom(new InternetAddress(FROM_NAME));
             message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
             message.setSubject("Purchase confirmation");
             message.setText(sb.toString());
+            Transport.send(message);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendRegistrationConfirmationEmail(String email) {
+        Session session = setupSession();
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(FROM_NAME));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(email));
+            message.setSubject("Registration confirmed");
+            message.setText("Your registration has been confirmed.\nThank you for registering!");
             Transport.send(message);
         } catch (MessagingException e) {
             e.printStackTrace();
