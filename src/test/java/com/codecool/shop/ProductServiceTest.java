021a1c222558a9c0344561a9ec883dc7e5a6dbd8
blippard <blippard@gmail.com>
2021-03-31 14:02:17 +0200

    Add: test for correct product ID call

--- src/test/java/com/codecool/shop/ProductServiceTest.java
+++ src/test/java/com/codecool/shop/ProductServiceTest.java
@@ -1,20 +1,65 @@
 package com.codecool.shop;
 
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.SupplierDao;
+import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
+import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.dao.implementation.SupplierDaoMem;
+import com.codecool.shop.model.Product;
+import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.model.Supplier;
+import com.codecool.shop.service.ProductService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ProductServiceTest {
+    private ProductDao productDao;
+    private ProductCategoryDao productCategoryDao;
+    private SupplierDao supplierDataStore;
+    private ProductCategory testProduct;
+    private Supplier testSupplier;
+
+    @BeforeEach
+    void generateProducts() {
+
+        this.productDao = ProductDaoMem.getInstance();
+        this.productCategoryDao = ProductCategoryDaoMem.getInstance();
+        this.supplierDataStore = SupplierDaoMem.getInstance();
+
+        this.testProduct = new ProductCategory("test",
+                "dept", "a description");
+        this.testSupplier = new Supplier("Supplier", "a description");
+        this.supplierDataStore.add(testSupplier);
+        this.productCategoryDao.add(testProduct);
+        this.productDao.add(new Product("Fluorite Tower",
+                15.99f, "USD",
+                "text", testProduct, testSupplier));
+    }
 
     @Test
     void getProductCategory_correctID_returnsID() {
+        ProductService testProductService = new ProductService(
+                this.productDao,
+                this.productCategoryDao);
+
+        assertEquals(
+                this.testProduct,
+                testProductService.getProductCategory(1));
 
     }
 
     @Test
-    void getProductCategory_incorrectID_throwsError() {
+    void getProductCategory_incorrectID_returnsNull() {
+        ProductService testProductService = new ProductService(
+                this.productDao,
+                this.productCategoryDao);
 
+        assertEquals(
+                null,
+                testProductService.getProductCategory(0));
     }
 
     @Test
