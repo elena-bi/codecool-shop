e164781dd19dd9fa737771a6833c6454d969fdf8
Solya Gergo <solya.gergo.sandor@gmail.com>
2021-03-31 13:46:17 +0200

    Add: CheckoutController sets the user as context variable if found in session

--- src/main/java/com/codecool/shop/controller/CheckoutController.java
+++ src/main/java/com/codecool/shop/controller/CheckoutController.java
@@ -3,6 +3,7 @@ package com.codecool.shop.controller;
 import com.codecool.shop.config.TemplateEngineUtil;
 import com.codecool.shop.dao.UserDetailsDao;
 import com.codecool.shop.dao.implementation.UserDetailsDaoMem;
+import com.codecool.shop.model.User;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -11,6 +12,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(urlPatterns = {"/Checkout"})
@@ -35,9 +37,15 @@ public class CheckoutController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
         WebContext context = new WebContext(request, response, request.getServletContext());
+
+        HttpSession session = request.getSession(false);
+        if (session != null || session.getAttribute("user") != null) {
+            User user = (User) session.getAttribute("user");
+            context.setVariable("user", user);
+        }
+
         engine.process("Checkout.html", context, response.getWriter());
     }
 
