b18c7e39d710fe8a0367dea01e1d634991e0c7c5
Solya Gergo <solya.gergo.sandor@gmail.com>
2021-03-31 15:00:12 +0200

    Add: login

--- src/main/java/com/codecool/shop/controller/LoginController.java
+++ src/main/java/com/codecool/shop/controller/LoginController.java
@@ -6,18 +6,23 @@ import com.codecool.shop.model.User;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.Optional;
 
+@WebServlet(urlPatterns = {"/login"})
 public class LoginController extends HttpServlet {
     private final UserDaoMem userDaoMem = UserDaoMem.getInstance();
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
-
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+        engine.process("login/login.html", context, resp.getWriter());
     }
 
     @Override
@@ -34,5 +39,13 @@ public class LoginController extends HttpServlet {
             return;
         }
         Optional<User> user = userDaoMem.getUserWithNamePassword(username, password);
+        if (user.isEmpty()) {
+            context.setVariable("failed", true);
+            engine.process("login/login.html", context, resp.getWriter());
+            return;
+        }
+        HttpSession session = req.getSession();
+        session.setAttribute("user", user.get().getId());
+        resp.sendRedirect("/");
     }
 }
