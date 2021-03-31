bf3542572142ba06d208cf70e29799d8a9b1f761
Solya Gergo <solya.gergo.sandor@gmail.com>
2021-03-31 10:37:09 +0200

    Add: RegistrationService register

--- src/main/java/com/codecool/shop/service/RegistrationService.java
+++ src/main/java/com/codecool/shop/service/RegistrationService.java
@@ -1,9 +1,13 @@
 package com.codecool.shop.service;
 
 import com.codecool.shop.dao.implementation.UserDaoMem;
+import com.codecool.shop.model.User;
+
+import java.util.Optional;
 
 public class RegistrationService {
     private UserDaoMem userDaoMem = UserDaoMem.getInstance();
+    private HashService hashService = HashService.getInstance();
     private static RegistrationService instance = null;
 
     private RegistrationService() {}
@@ -13,5 +17,15 @@ public class RegistrationService {
             instance = new RegistrationService();
         }
         return instance;
+    }
+
+    public boolean register(String username, String email, String password) {
+        if (userDaoMem.isNameUsed(username) || userDaoMem.isEmailUsed(email) || userDaoMem.isPasswordUsed(password)) return false;
+        byte[] salt = hashService.getNewSalt();
+        hashService.hashPassword(password, salt);
+        User user = new User(userDaoMem.getCurrentId(), username, password, email, salt);
+        userDaoMem.incrementCurrentId();
+        userDaoMem.addUser(user);
+        return true;
     }
 }
